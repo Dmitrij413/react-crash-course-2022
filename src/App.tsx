a8fe4a5c7ff388b25c91d3001727d22a650ed126
Dmitrij413 <dmitrij.khramov@bk.ru>
2023-04-17 18:20:06 +0400

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   const [modal, setModal] = useState(true)
   
   return(
-    <div className='container mx-auto max-w-2xl pt-5'>
+    <div className='container mx-auto max-w-2xl pb pt-5'>
       { loading && <Loader /> }
       { error && <ErrorMessage error={ error }/> }
       { products.map(product => <Product product={product} key={product.id} />)}

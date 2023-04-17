500983769d71cfb81d245d392a69368c82cfc533
Dmitrij413 <dmitrij.khramov@bk.ru>
2023-04-17 23:23:08 +0400

    Changes to be committed: 	modified:   src/App.tsx 	modified:   src/components/CreateProduct.tsx 	modified:   src/hooks/products.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useProducts } from './hooks/products';
-import {Loader} from './components/Loader';
-import {ErrorMessage} from './components/ErrorMessage';
+import { Loader } from './components/Loader';
+import { ErrorMessage } from './components/ErrorMessage';
 import { Product } from './components/Product';
 import { Modal } from './components/Modal';
 import { CreateProduct } from './components/CreateProduct';
 import { useState } from 'react';
+import { IProduct } from './models';
 
 function App() {
-  const { loading, error, products } = useProducts()
+  const { loading, error, products, addProduct } = useProducts()
   const [modal, setModal] = useState(true)
+
+  const createHandler = (product: IProduct) => {
+    setModal(false)
+      
+  }
   
   return(
     <div className='container mx-auto max-w-2xl pb pt-5'>
@@ -17,7 +23,7 @@ function App() {
       { products.map(product => <Product product={product} key={product.id} />)}
 
       {modal && <Modal title='Create new product'>
-        <CreateProduct onCreate={() => setModal(false)}/>
+        <CreateProduct onCreate={createHandler}/>
       </Modal>}
     </div>
   )

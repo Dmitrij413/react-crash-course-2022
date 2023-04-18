602c385124ec5229be97fb62df916ee41d326a51
Dmitrij413 <dmitrij.khramov@bk.ru>
2023-04-18 18:26:18 +0400

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	new file:   src/components/Navigation.tsx 	modified:   src/index.tsx 	new file:   src/pages/AboutPage.tsx 	new file:   src/pages/ProductPage.tsx

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage() {
+	return (
+		<div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta aliquam vitae cum iusto, doloribus facere officiis illo saepe cupiditate, voluptates porro amet maiores. Illum vel quod blanditiis consectetur exercitationem optio repellat, dolor in suscipit natus omnis quidem ullam magni culpa unde! Assumenda temporibus repellendus quo corporis accusamus iste reiciendis quisquam.</div>
+	)
+}
